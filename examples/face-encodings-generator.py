a941fc8ce05e9b7326e4680f4691374717140f86
ben-wycliff <ben12wycliff@gmail.com>
2020-05-03 23:59:42 +0300

    modifies folder structure

---
+++ examples/face-encodings-generator.py
@@ -0,0 +1,24 @@
+# load necessary libraries
+import face_recognition, glob
+import csv
+
+folders = glob.glob('../data/known/*')
+columns = ["fe"+str(i+1) for i in range(128)]
+case = 0
+
+with open('../data/images.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Case#', 'name', *columns])
+    for i, folder in enumerate(folders):
+        files = glob.glob(folder + '/*')
+        for file in files:
+            image = face_recognition.load_image_file(file)
+            face_location = face_recognition.face_locations(image, number_of_times_to_upsample=2)
+            known_face_encoding = face_recognition.face_encodings(image, known_face_locations=face_location)[0]
+            case += 1
+            writer.writerow([case, folder.split('\\')[-1], *known_face_encoding])
+    # known.append(
+    #     {
+    #         "name": file[26:-5],
+    #         "face_encoding": known_face_encoding
+    #     })
